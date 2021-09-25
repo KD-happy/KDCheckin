019ff9b5d5ea01e3116c6bb80bcc0d16bc1f40af
KD-happy <2507086134@qq.com>
2021-09-25 09:15:22 +0800

    KDconfig的配置问题

--- KDconfig.py
+++ KDconfig.py
@@ -5,7 +5,7 @@ def getYmlConfig(yaml_file='Cookie.yml'):
     cookie_file = '/ql/config/Cookie.yml'
     if os.path.exists(ql_file):
         print('当前环境为: 青龙面板')
-        if os.path.exists(ql_file):
+        if os.path.exists(cookie_file):
             file = open(cookie_file, 'r', encoding="utf-8")
         else:
             print('未找到Cookie的配置文件\n请执行下面的命令行\ncp')
