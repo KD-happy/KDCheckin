20fa23ef3f80e67b57dcc20c6d994655c608fbe3
KD-happy <2507086134@qq.com>
2021-09-29 21:27:26 +0800

    添加 signList 签到列表

--- QianDao.py
+++ QianDao.py
@@ -9,11 +9,14 @@ from KDconfig import getYmlConfig, send
 from KDsrc.more import map
 
 all = StringIO()
-all.write('【今日签到】\n')
+all.write('===== 每日签到 =====\n')
 config = getYmlConfig()
+signList = config.get('signList', '')
 
-for name, sign in map.items():
+# for name, sign in map.items():
+for name in signList.split(', '):
     try:
+        sign = map.get(name)
         if config.get(name) != None:
             print(f'{sign[0]} 开始签到...')
             if config.get(name).get('cookies') != None:
@@ -25,5 +28,7 @@ for name, sign in map.items():
                 all.write(f'{sign[0]} 没有配置文件\n')
     except:
         print(traceback.format_exc())
-print(all.getvalue())
-send('今日签到', all.getvalue().replace('\n\n', ''))
+else:
+    all.write('\n')
+print(all.getvalue().replace('\n\n\n', ''))
+send('每日签到', all.getvalue().replace('\n\n\n', ''))
