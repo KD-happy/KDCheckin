1045ee15edf5beae1c30582d43b109f5253b802f
KD-happy <2507086134@qq.com>
2021-09-29 20:43:31 +0800

    2021年09月29日20时41分27秒

--- KDsrc/more.py
+++ KDsrc/more.py
@@ -9,6 +9,12 @@ from KDsrc.Cloud import Cloud
 from KDsrc.CSDN import CSDN
 from KDsrc.Egame import Egame
 from KDsrc.EnShan import EnShan
+from KDsrc.Heytap import Heytap
+from KDsrc.HLX import HLX
+from KDsrc.Lenovo import Lenovo
+from KDsrc.NoteYouDao import NoteYouDao
+from KDsrc.PTA import PTA
+from KDsrc.W2PJ import W2PJ
 
 map = {
     "AcFun": ["AcFun", AcFun],
@@ -19,4 +25,10 @@ map = {
     "CSDN": ["CSDN", CSDN],
     "Egame": ["企鹅电竞", Egame],
     "EnShan": ["恩山论坛", EnShan],
+    "Heytap": ["欢太商城", Heytap],
+    "HLX": ["葫芦侠", HLX],
+    "Lenovo": ["联想商城", Lenovo],
+    "NoteYouDao": ["有道云笔记", NoteYouDao],
+    "PTA": ["PTA", PTA],
+    "W2PJ": ["吾爱破解", W2PJ]
 }
