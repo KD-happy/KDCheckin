96ea5666bd0b0b7e6536e1b5200d7d74c786a1c8
KD-happy <2507086134@qq.com>
2021-10-02 21:33:03 +0800

    添加网易阅读

--- KDsrc/more.py
+++ KDsrc/more.py
@@ -3,10 +3,12 @@
 """
 from KDsrc.AcFun import AcFun
 from KDsrc.AZG import AZG
+from KDsrc.BDTieBa import BDTieBa
 from KDsrc.BLBL import BLBL
 from KDsrc.CCAVA import CCAVA
 from KDsrc.Cloud import Cloud
 from KDsrc.CSDN import CSDN
+from KDsrc.Du163 import Du163
 from KDsrc.Egame import Egame
 from KDsrc.EnShan import EnShan
 from KDsrc.Heytap import Heytap
@@ -14,16 +16,19 @@ from KDsrc.HLX import HLX
 from KDsrc.Lenovo import Lenovo
 from KDsrc.NoteYouDao import NoteYouDao
 from KDsrc.PTA import PTA
+from KDsrc.SMZDM import SMZDM
 from KDsrc.ToolLu import ToolLu
 from KDsrc.W2PJ import W2PJ
 
 map = {
     "AcFun": ["AcFun", AcFun],
     "AZG": ["爱助攻", AZG],
+    "BDTieBa": ["百度贴吧", BDTieBa],
     "BLBL": ["哔哩哔哩", BLBL],
     "CCAVA": ["CCAVA", CCAVA],
     "Cloud": ["天翼云盘", Cloud],
     "CSDN": ["CSDN", CSDN],
+    "Du163": ["网易读书", Du163],
     "Egame": ["企鹅电竞", Egame],
     "EnShan": ["恩山论坛", EnShan],
     "Heytap": ["欢太商城", Heytap],
@@ -31,6 +36,7 @@ map = {
     "Lenovo": ["联想商城", Lenovo],
     "NoteYouDao": ["有道云笔记", NoteYouDao],
     "PTA": ["PTA", PTA],
+    "SMZDM": ["什么值得买", SMZDM],
     "ToolLu": ["Tool工具", ToolLu],
     "W2PJ": ["吾爱破解", W2PJ]
 }
