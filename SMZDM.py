368f488aa93e15a29d764d9ef7ee46f1979081dd
KD-happy <2507086134@qq.com>
2021-10-02 12:49:32 +0800

    什么值得买

--- SMZDM.py
+++ SMZDM.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 cron: 55 7 * * *
-new Env('{签到的标题}');
+new Env('什么值得买');
 """
 
 import requests, time, re, json, sys, traceback
@@ -48,8 +48,8 @@ class SMZDM:
         return msg
 
     def SignIn(self):
-        print("【{签到的标题} 日志】")
-        self.sio.write("【{签到的标题}】\n")
+        print("【什么值得买 日志】")
+        self.sio.write("【什么值得买】\n")
         for cookie in self.Cookies:
             cookie = cookie.get("user")
             print(f"{cookie.get('name')} 开始签到...")
@@ -92,11 +92,11 @@ if __name__ == '__main__':
             sio = smzdm.SignIn()
             print(f'\n{sio.getvalue()}')
             if Cookies.get('send') != None and Cookies['send'] == 1:
-                send('{签到的标题}', sio.getvalue())
+                send('什么值得买', sio.getvalue())
             else:
                 print('推送失败: 关闭了推送 or send配置问题')
         else:
-            print('配置文件 {签到的标题} 没有 "cookies"')
+            print('配置文件 什么值得买 没有 "cookies"')
             sys.exit()
     else:
-        print('配置文件没有 {签到的标题}')
+        print('配置文件没有 什么值得买')
